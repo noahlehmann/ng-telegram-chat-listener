4a977ee094be70849ad0d0f1fa1203e3b4c5cf30
Noah Lehmann <noah.lehmann@protonmail.com>
2022-11-30 17:12:23 +0100

    feature/chat-fe: set env variable to groupname of bot

--- src/app/component/chat-feed/chat-feed.component.ts
+++ src/app/component/chat-feed/chat-feed.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import ChatMessageModel from "../../model/chat-message.model";
 import {TelegramService} from "../../util/telegram.service";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-chat-feed',
@@ -20,7 +21,7 @@ export class ChatFeedComponent implements OnInit {
         res => {
           this.messages = res.result.filter(res => {
             return res.message != undefined
-              && res.message.chat.title == "Boom WS 22"
+              && res.message.chat.title == environment.groupName
               && res.message.text != undefined
           });
         }
