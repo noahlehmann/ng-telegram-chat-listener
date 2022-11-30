298a240362d4922cf334d39263111e79f7583570
Noah Lehmann <noah.lehmann@protonmail.com>
2022-11-30 11:40:01 +0100

    feature/chat-fe: insert basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatMessageComponent } from './component/chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/model/chat-message.model.ts
@@ -0,0 +1,16 @@
+export default interface ChatMessageModel {
+  message_id: number;
+  from: {
+    id: number;
+    is_bot: boolean;
+    first_name: string;
+    language_code: string;
+  }
+  chat: {
+    id: number;
+    type: string;
+    first_name: string;
+  }
+  date: number;
+  text: string;
+}
